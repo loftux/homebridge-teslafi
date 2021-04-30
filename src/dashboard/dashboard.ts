3acf81c883d8413ae54ecaa4b1d2ad6fbf025e8f
Peter Löfgren <peter@loftux.se>
2021-04-30 04:46:47 +0200

    FIX: Do not show 'No Tagged Location Found' on dashboard

--- src/dashboard/dashboard.ts
+++ src/dashboard/dashboard.ts
@@ -69,7 +69,8 @@ export class Dashboard {
       // Add the location if present. Sometimes TeslaFi includes that in the notes already, so check for that
       if (
         this.teslacar.location &&
-        notes.indexOf(this.teslacar.location) === -1
+        notes.indexOf(this.teslacar.location) === -1 &&
+        this.teslacar.location !== 'No Tagged Location Found'
       ) {
         notes = notes + ' @\u200A' + this.teslacar.location;
       }
